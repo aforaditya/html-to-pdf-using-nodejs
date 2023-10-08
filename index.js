b56cb64fe2cf77dd5f06c15853b727ffc2db910c
Aditya Gupta <75029064+aforaditya@users.noreply.github.com>
2023-10-08 07:24:46 +0000

    New code

--- index.js
+++ index.js
@@ -1,100 +1,88 @@
-const fs = require('fs')
-const path = require('path')
-const utils = require('util')
-const puppeteer = require('puppeteer')
-const hb = require('handlebars')
-const readFile = utils.promisify(fs.readFile)
-const express = require('express')
-const app = express()
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const express = require('express');
 const archiver = require('archiver');
 
+const readFile = util.promisify(fs.readFile);
+const app = express();
+
 async function getTemplateHtml() {
-console.log("Loading template file in memory")
-try {
-const invoicePath = path.resolve("./template.html");
-return await readFile(invoicePath, 'utf8');
-} catch (err) {
-return Promise.reject("Could not load html template");
-}
-}
-
-
-async function generatePdf(challans, resx) {
-
-
-getTemplateHtml().then(async (res) => {
-// Now we have the html code of our template in res object
-// you can check by logging it on console
-// console.log(res)
-console.log("Compiing the template with handlebars")
-const template = hb.compile(res, { strict: true });
-// we are using headless mode
-const browser = await puppeteer.launch();
-const page = await browser.newPage()
-
-
-for(let i=0; i<challans.length; i++){
-    // we have compile our code with handlebars
-    const result = template(challans[i]['challanData']);
-    // We can use this to add dyamic data to our handlebas template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
-    const html = result;
-    // We set the page content as the generated html by handlebars
-    await page.setContent(html)
-    // We use pdf function to generate the pdf in the same folder as this file.
-    await page.pdf({ path: `${challans[i]['challanNumber']}.pdf`, format: 'A4' })
+  console.log("Loading template file in memory");
+  try {
+    const invoicePath = path.resolve("./template.html");
+    return await readFile(invoicePath, 'utf8');
+  } catch (err) {
+    console.error("Could not load html template", err);
+    throw err;
+  }
 }
 
+async function generatePdf(challans, res) {
+  try {
+    const templateHtml = await getTemplateHtml();
+    console.log("Compiling the template with handlebars");
+    const template = handlebars.compile(templateHtml, { strict: true });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-await browser.close();
-console.log("PDF Generated")
-zip(resx)
-
-}).catch(err => {
-console.error(err)
-});
-}
-
-
-let challans = [
-    {
-        challanNumber: 10011,
-        challanData: {
-            "ContactPersonName": "Dummy Value",
-            "DeliveryChallanNumber": "Dummy Value",
-            "DeliveryChallanDate": "Dummy Value",
-            "ReceiverName": "Dummy Value",
-            "ConsigneeName": "Dummy Value",
-            "ReceiverAddress": "Dummy Value",
-            "SpokesPersonName": "Dummy Value",
-            "ConsigneeMobileNumber": "Dummy Value",
-            "ReceiverCity": "Dummy Value",
-            "ConsigneeAddress": "Dummy Value",
-            "ReceiverStateAndPin": "Dummy Value",
-            "ConsigneeCity": "Dummy Value",
-            "ConsigneeStateAndPin": "Dummy Value",
-            "GSTINNumber": "Dummy Value",
-            "ShipToState": "Dummy Value",
-            "StateCode": "Dummy Value",
-            "PONumber": "Dummy Value",
-            "ProductDescription": "Dummy Value",
-            "HSNCode": "Dummy Value",
-            "MOQ": "Dummy Value",
-            "Boxes": "Dummy Value",
-            "Quantity": "Dummy Value",
-            "UnitPrice": "Dummy Value",
-            "TotalAmount": "Dummy Value",
-            "CGST": "Dummy Value",
-            "SGST": "Dummy Value",
-            "IGSTValue": "Dummy Value",
-            "IGST": "Dummy Value",
-            "Total": "Dummy Value",
-            "TotalPreTax": "Dummy",
-          }          
+    for (let i = 0; i < challans.length; i++) {
+      const result = template(challans[i].challanData);
+      const html = result;
+      await page.setContent(html);
+      await page.pdf({ path: `${challans[i].challanNumber}.pdf`, format: 'A4' });
     }
-]
 
+    await browser.close();
+    console.log("PDFs Generated");
+    zipAndSend(res);
 
-function deleteFile(fileName) {
+  } catch (err) {
+    console.error("Error generating PDFs", err);
+    res.status(500).send('Error generating PDFs');
+  }
+}
+
+async function zipAndSend(res) {
+  try {
+    console.log("Zipping PDFs");
+    const filesToZip = challans.map(c => `${c.challanNumber}.pdf`);
+    const zipFileName = 'challan.zip';
+    const output = fs.createWriteStream(zipFileName);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+
+    archive.pipe(output);
+
+    filesToZip.forEach((file) => {
+      archive.file(file, { name: file });
+    });
+
+    archive.finalize();
+
+    output.on('close', () => {
+      console.log("Zipped");
+      res.download(zipFileName, (err) => {
+        if (err) {
+          console.error('Error downloading the ZIP file:', err);
+          res.status(500).send('Error downloading the ZIP file');
+        } else {
+          console.log('ZIP file downloaded');
+          deleteFiles(filesToZip);
+        }
+      });
+    });
+
+  } catch (err) {
+    console.error("Error zipping PDFs", err);
+    res.status(500).send('Error zipping PDFs');
+  }
+}
+
+function deleteFiles(files) {
+    files.push('challan.zip')
+  files.forEach((fileName) => {
     fs.unlink(fileName, (err) => {
       if (err) {
         console.error(`Error deleting ${fileName}:`, err);
@@ -102,70 +90,52 @@ function deleteFile(fileName) {
         console.log(`${fileName} has been deleted.`);
       }
     });
-  }
-
-async function zip(resx){
-
-    console.log("Zipping")
-    const filesToZip = challans.map(c=> `${c.challanNumber}.pdf`)
-    console.log(filesToZip);
-
-            // Create a ZIP archive
-        const zipFileName = 'challan.zip';
-        const output = fs.createWriteStream(zipFileName);
-        const archive = archiver('zip', {
-        zlib: { level: 9 }, // Compression level (0-9)
-        });
-
-        // Pipe the archive to the output file
-        archive.pipe(output);
-
-        // Add files to the archive
-        filesToZip.forEach((file) => {
-        archive.file(file, { name: file });
-        });
-
-        // Finalize the archive
-        archive.finalize();
-        console.log("Zipped")
-        filesToZip.forEach((fileName) => {
-            deleteFile(fileName);
-          });
-
-
-        setInterval(()=>sendZip(resx), 3000)
-        
-        
-        
+  });
 }
 
+let challans = [
+    {
+      challanNumber: 10011,
+      challanData: {
+        ContactPersonName: "John Doe",
+        DeliveryChallanNumber: "CH10011",
+        DeliveryChallanDate: "2023-10-08",
+        ReceiverName: "Jane Smith",
+        ConsigneeName: "ABC Company",
+        ReceiverAddress: "123 Main St",
+        SpokesPersonName: "Sam Brown",
+        ConsigneeMobileNumber: "555-123-4567",
+        ReceiverCity: "Cityville",
+        ConsigneeAddress: "456 Oak St",
+        ReceiverStateAndPin: "State A, 12345",
+        ConsigneeCity: "Townsville",
+        ConsigneeStateAndPin: "State B, 54321",
+        GSTINNumber: "GSTIN12345",
+        ShipToState: "State C",
+        StateCode: "6789",
+        PONumber: "PO-1001",
+        ProductDescription: "Sample Product",
+        HSNCode: "HSN-1234",
+        MOQ: "10",
+        Boxes: "5",
+        Quantity: "50",
+        UnitPrice: "$20",
+        TotalAmount: "$1000",
+        CGST: "$50",
+        SGST: "$50",
+        IGSTValue: "$0",
+        IGST: "$0",
+        Total: "$1100",
+        TotalPreTax: "$1000",
+      },
+    },
+  ];
+  
 
-function sendZip(res){
-    console.log('Sending');
-    res.download('challan.zip', (err) => {
-        if (err) {
-          res.status(500).send('Error downloading the ZIP file');
-        } else {
-          // Delete the zip file after it has been sent
-          fs.unlink('challan.zip', (unlinkError) => {
-            if (unlinkError) {
-              console.error('Error deleting ZIP file:', unlinkError);
-            }
-          });
-        }
-      });
-}
+app.get('/generate', async (req, res) => {
+  generatePdf(challans, res);
+});
 
-function generate(res){
-     generatePdf(challans, res);
-     console.log('IN generate');
-}
-
-app.get('/generate', async (req, res)=>{
-    generate(res)
-})
-
-
-app.listen(3000, ()=>console.log('Server ON'))
-
-
+app.listen(3000, () => {
+  console.log('Server ON');
+});
